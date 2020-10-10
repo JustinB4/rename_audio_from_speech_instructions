aaa4daa55fafbe8b9f6e3ab5f7d86fbd8889c1b1
Justin Ben <justin.ben@gmail.com>
2020-10-09 18:33:01 -0700

    Update Speech To Text File Rename.py

--- Speech To Text File Rename.py
+++ Speech To Text File Rename.py
@@ -8,10 +8,10 @@ for filename in os.listdir(os.getcwd()):
         test = sr.AudioFile(filename)
         with test as source:
             audio = r.record(source, duration=2)
-        #try:
-        result = r.recognize_sphinx(audio)
-        newname = os.path.splitext(filename)[0] + '-' + result + os.path.splitext(filename)[1]
-        print (newname)
-        os.rename(filename, newname)
-        #except:
-        #    pass
+        try:
+            result = r.recognize_sphinx(audio)
+            newname = os.path.splitext(filename)[0] + '-' + result + os.path.splitext(filename)[1]
+            print (newname)
+            os.rename(filename, newname)
+        except:
+            pass
